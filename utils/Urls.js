233b4d21f33cca785b7d6bcf2be181e2842c02a5
Huynh Thong <thonghuynh@aiontech.vn>
2022-11-28 00:25:28 +0700

    change http -> https

--- utils/Urls.js
+++ utils/Urls.js
@@ -1,5 +1,5 @@
 export default function Urls(){
-    const BASE_URL = "http://api-dev-accounts.aigoox.com/"
+    const BASE_URL = "https://api-dev-accounts.aigoox.com/"
     const BASE_URL_RESOURCES = "https://resources.aigoox.com/";
     const API = BASE_URL + "api/"
 
